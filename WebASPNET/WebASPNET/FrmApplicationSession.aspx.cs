4f2682665a62249aa08501c7c0c4fdcc1160c14a
neighborpil <neighborpil@naver.com>
2017-07-20 09:03:29 +0900

    lec 2 - 12 finished

---
+++ WebASPNET/WebASPNET/FrmApplicationSession.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebASPNET
+{
+    public partial class FrmApplicationSession : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Application 전역변수 : Public 한 전역변수
+            // Session 전역변수 : private한 전역변수
+            if (Application["Count"] == null)
+            {
+                Application.Lock();
+                Application["Count"] = 1;
+                Application.UnLock();
+            }
+            else
+                Application["Count"] = (int)Application["Count"] + 1;
+
+            if (Session["Count"] == null)
+                Session["Count"] = 1;
+            else
+                Session["Count"] = (int)Session["Count"] + 1;
+
+            lblApplication.Text = Application["Count"].ToString();
+            lblSession.Text = Session["Count"].ToString();
+            this.lblSessionID.Text = Session.SessionID;
+            lblTimeout.Text = Session.Timeout.ToString(); //세션 유지시간은 기본 20분
+
+        }
+    }
+}
---
+++ WebASPNET/WebASPNET/FrmServerExecute.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WebASPNET
+{
+    public partial class FrmServerExecute : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // 현재 웹폼에 또 다른 웹폼을 추가 : 제어권 돌어옴
+            Server.Execute("./FrmRequest.aspx");
+            Server.Execute("./FrmRequestUserHostAddress.aspx");
+
+            // 현재 웹폼에 또 다른 웹 폼을 추가 : 제어권 넘김
+            Server.Transfer("./FrmResponseBuffer.aspx"); //이거 아래는 실행 안됨
+
+            //Transfer() == Execute() + Response.End()
+            // 아래 구문은 실행 안됨
+            Response.Write("Test");
+        }
+    }
+}
