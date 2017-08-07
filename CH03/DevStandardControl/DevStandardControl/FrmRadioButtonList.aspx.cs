4eb5ed1de12aaa663b7f4892c22c10c2d1a26662
neighborpil <neighborpil@naver.com>
2017-08-07 23:44:58 +0900

    lec3_24 finished

---
+++ CH03/DevStandardControl/DevStandardControl/FrmRadioButtonList.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevStandardControl
+{
+    public partial class FrmRadioButtonList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Empty
+        }
+
+        protected void btnOK_Click(object sender, EventArgs e)
+        {
+            string strMsg = ""; // Empty
+            if(lstMarried.SelectedItem.Text == "미혼")
+            {
+                strMsg = "미혼 선택";
+            }
+            else
+            {
+                strMsg = lstMarried.SelectedItem.Text + " 선택";
+            }
+            Response.Write($"{strMsg} <br>");
+        }
+    }
+}
