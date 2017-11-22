7a3d1226d99e6f954a3273c0b737b90c2322d5c6
neighborpil <neighborpil@naver.com>
2017-11-22 09:56:36 +0900

    lec5_8 start 03:50

---
+++ DevWebForms/DevWebForms/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DevWebForms.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DevWebForms/DevWebForms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevWebForms.Global" Language="C#" %>
